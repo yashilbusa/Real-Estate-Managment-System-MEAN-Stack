0a771a357201699cfa18a2f98c42395fa9f169fb
yashilbusa <yashilpatel1914@gmail.com>
2025-03-19 12:06:49 +0530

    first commit

---
+++ backend/index.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import cors from 'cors';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import bodyparser from 'body-parser';
+mongoose.set('debug',true);
+
+dotenv.config();
+const app = express();
+const port = process.env.PORT;
+
+app.use(express.json());
+app.use(cors());
+app.use(bodyparser.json());
+
+mongoose.connect(process.env.url)
+    .then(() => console.info('Connected to MongoDB'))
+    .catch(err => console.error('Error Connecting to MongoDB', err));
+
+app.listen(port, () => {
+    console.info(`Server Running on http://localhost:${port}`);
+});
