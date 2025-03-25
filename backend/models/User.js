c40ed13bda17e6db7b8de18054cc59726c962576
yashilbusa <yashilpatel1914@gmail.com>
2025-03-25 16:06:04 +0530

    commit

--- backend/models/User.js
+++ backend/models/User.js
@@ -3,14 +3,13 @@ import mongoose from 'mongoose';
 // const roleMapping = {
 //     0: "buyer",
 //     1: "seller",
-//     2: "agent"
 // }
 
 const userSchema = new mongoose.Schema({
     name: { type: String, required: true, unique: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    role: { type:String,enum:["buyer","seller","agent","admin"],default:"buyer", required: true},
+    role: { type:String,enum:["buyer","seller","admin"],default:"buyer", required: true},
     token: { type: String }
 });
 
