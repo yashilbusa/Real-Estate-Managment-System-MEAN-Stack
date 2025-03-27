b0299aebd81402cbf28c7a245fe16d7f2974ae4f
yashilbusa <yashilpatel1914@gmail.com>
2025-03-27 17:16:22 +0530

    commit

--- backend/controllers/approveController.js
+++ backend/controllers/approveController.js
@@ -2,7 +2,13 @@ import ApproveRequest from "../models/ApproveRequest.js";
 
 export const buyPropertyRequest = async (req,res) =>{
     try {
-        res.json({ message: "Request Send from Backend"});
+        const { sellerId } =req.body;
+        const request = new ApproveRequest({
+            buyerId,
+            sellerId,
+            status
+        })
+        // res.json({ message: "Request Send from Backend"});
     } catch (error) {
         res.status(500).json({ message: 'Server error', error: error.message });  
     }
