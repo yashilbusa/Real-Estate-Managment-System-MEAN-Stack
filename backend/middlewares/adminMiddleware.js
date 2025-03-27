7759d4a0e899adc24b4c2c195c05889876fc4210
yashilbusa <yashilpatel1914@gmail.com>
2025-03-27 10:15:18 +0530

    c

--- backend/middlewares/adminMiddleware.js
+++ backend/middlewares/adminMiddleware.js
@@ -1,11 +1,8 @@
 import jwt from 'jsonwebtoken';
 
 export const adminMiddleware = (req, res, next) => {
-  const token = req.headers.authorization?.split(' ')[1]; 
-
-  if (!token) {
-    return res.status(401).json({ message: 'No token provided' });
-  }
+  const token = req.header('Authorization')?.replace('Bearer ', '');
+        if (!token) return res.status(401).json({ error: 'No token provided' });
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
