f219d02de4a49a11b1a5664464f535fcb1576fae
yashilbusa <yashilpatel1914@gmail.com>
2025-03-27 16:06:51 +0530

    c

--- backend/models/ApproveRequest.js
+++ backend/models/ApproveRequest.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 const approveRequestSchema = new mongoose.Schema({
     buyerId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
     sellerId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-    status: { type:String, enum: ['pending', 'approved', 'rejected']}
+    status: { type:String, enum: ['pending', 'approved', 'rejected'], default: 'pending'}
 })
 
 const ApproveRequest = mongoose.model('ApproveRequest', approveRequestSchema);
