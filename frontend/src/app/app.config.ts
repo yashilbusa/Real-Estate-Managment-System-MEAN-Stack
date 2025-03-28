0ef42a768a6300573824a0d62729d927a4eac5af
yashilbusa <yashilpatel1914@gmail.com>
2025-03-28 18:05:24 +0530

    commit

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -4,22 +4,22 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideHttpClient, withFetch } from '@angular/common/http';
-import { GoogleSigninService } from './services/google-sign-in.service';
+// import { GoogleSigninService } from './services/google-sign-in.service';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes), 
     provideClientHydration(),
     provideHttpClient(),
-    provideHttpClient(withFetch()),
-    GoogleSigninService,
-    {
-      provide: 'SocialAuthServiceConfig',
-      useValue: {
-        GoogleLoginProvider: {
-          clientId: '267275564598-nmpit5croecbv2a36r0p1g1f04m56e8t.apps.googleusercontent.com', 
-        }
-      }
-    }
+    provideHttpClient(withFetch())
+    // GoogleSigninService,
+    // {
+    //   provide: 'SocialAuthServiceConfig',
+    //   useValue: {
+    //     GoogleLoginProvider: {
+    //       clientId: '267275564598-nmpit5croecbv2a36r0p1g1f04m56e8t.apps.googleusercontent.com', 
+    //     }
+    //   }
+    // }
   ]
 };
